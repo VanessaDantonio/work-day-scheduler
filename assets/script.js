06e17c95c86f7a354c6d13b61b9d3e041903b7db
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-01-30 10:40:27 +0000

    add function to set each time-block color

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,25 @@ $(document).ready(function() {
 const currentDateEl = moment().format('dddd, MMMM Do YYYY');
 // Set the content of #currentDay
 $('#currentDay').html(currentDateEl);
+
+// Set colors for time-blocks for past, present and future
+function timeColor() {
+    let currentTime = moment().hour();
+
+    $('.time-block').each(function () {
+        var blockTime = parseInt($(this).attr('id').split('hour-')[1]);
+
+        // Condition to set blockTime colors
+        if (currentTime > blockTime) {
+            $(this).addClass('past');
+        }
+        else if (currentTime === blockTime) {
+            $(this).addClass('present');
+        }
+        else {
+            $(this).addClass('future');
+        }
+    });
+};
+timeColor();
 });
