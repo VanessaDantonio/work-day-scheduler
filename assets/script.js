c497bbc1bc63de29c3e957c65c788dc5f68788f9
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-01-31 23:32:05 +0000

    save and load events from localStorage

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@ const currentDateEl = moment().format('dddd, MMMM Do YYYY');
 // Set the content of #currentDay
 $('#currentDay').html(currentDateEl);
 
+renderText();
+
 // Set colors for time-blocks for past, present and future
 function timeColor() {
     let currentTime = moment().hour();
@@ -24,5 +26,28 @@ function timeColor() {
         }
     });
 };
+
 timeColor();
+
+// Save user event input and the referenced time-block to local storage
+$('.saveBtn').click(function (event) {
+    event.preventDefault();
+    let eventInput = $(this).siblings('.description').val();
+    let eventHour = $(this).parent().attr('id');
+    localStorage.setItem(eventHour, eventInput);
 });
+
+// Loads events from local storage
+function renderText() {
+    $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+};
+})
